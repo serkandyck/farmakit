7fc6850d2bceb2b35dc350a37c45ce2501eaed0e
Serkan Dayıcık <serkandayicik@gmail.com>
2019-12-10 15:16:43 +0300

    olcum test

--- app/Http/Controllers/OlcumController.php
+++ app/Http/Controllers/OlcumController.php
@@ -20,48 +20,45 @@ class OlcumController extends Controller
     	if(!$request->kod == 'Fk3306') {
     		return response()->json(['status' => 'Unauthorized.']);
     	}
+        $olcumDolap = new Olcum();
+        $sensorTip = SensorType::where('baslik', 'Dolap')->first();
+        $olcumDolap->type_id = $sensorTip->id;
+        $olcumDolap->cihaz_id = $request->id;
+        $olcumDolap->deger = $request->dolap;
+        $olcumDolap->tarih = Carbon::now();
+        $olcumDolap->save();
 
+        $olcumNem = new Olcum();
+        $sensorTip = SensorType::where('baslik', 'Nem')->first();
+        $olcumNem->type_id = $sensorTip->id;
+        $olcumNem->cihaz_id = $request->id;
+        $olcumNem->deger = $request->nem;
+        $olcumNem->tarih = Carbon::now();
+        $olcumNem->save();
+
+        $olcumOrtam = new Olcum();
+        $sensorTip = SensorType::where('baslik', 'Ortam')->first();
+        $olcumOrtam->type_id = $sensorTip->id;
+        $olcumOrtam->cihaz_id = $request->id;
+        $olcumOrtam->deger = $request->ortam;
+        $olcumOrtam->tarih = Carbon::now();
+        $olcumOrtam->save();
         if(!$request->id == 'nan') {
             if(!$request->dolap == 'nan') {
-                $olcumDolap = new Olcum();
-                $sensorTip = SensorType::where('baslik', 'Dolap')->first();
-                $olcumDolap->type_id = $sensorTip->id;
-                $olcumDolap->cihaz_id = $request->id;
-                $olcumDolap->deger = $request->dolap;
-                $olcumDolap->tarih = Carbon::createFromFormat('Y-m-d H:i:s', $request->tarih. ' '. $request->saat)->toDateTimeString();
-                if($olcumDolap->tarih < '2019-01-01') {
-                    $olcumDolap->tarih = Carbon::now();
-                }
-                $olcumDolap->save();
+                
             }
 
             if(!$request->nem == 'nan') {
-                $olcumNem = new Olcum();
-                $sensorTip = SensorType::where('baslik', 'Nem')->first();
-                $olcumNem->type_id = $sensorTip->id;
-                $olcumNem->cihaz_id = $request->id;
-                $olcumNem->deger = $request->nem;
-                $olcumNem->tarih = Carbon::createFromFormat('Y-m-d H:i:s', $request->tarih. ' '. $request->saat)->toDateTimeString();
-                if($olcumNem->tarih < '2019-01-01') {
-                    $olcumNem->tarih = Carbon::now();
-                }
-                $olcumNem->save();
+                
             }
 
             if(!$request->ortam == 'nan') {
-                $olcumOrtam = new Olcum();
-                $sensorTip = SensorType::where('baslik', 'Ortam')->first();
-                $olcumOrtam->type_id = $sensorTip->id;
-                $olcumOrtam->cihaz_id = $request->id;
-                $olcumOrtam->deger = $request->ortam;
-                $olcumOrtam->tarih = Carbon::createFromFormat('Y-m-d H:i:s', $request->tarih. ' '. $request->saat)->toDateTimeString();
-                if($olcumOrtam->tarih < '2019-01-01') {
-                    $olcumOrtam->tarih = Carbon::now();
-                }
-                $olcumOrtam->save();
+                
             }
 
-            return response()->json(['status' => 'Successfull.']);
+            
         }
+
+        return response()->json(['status' => 'Successfull.']);
     }
 }
